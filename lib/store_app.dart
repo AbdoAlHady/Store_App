49b1c5a1535196af1cff0913ca05674d54b2b22c
AbdoAlhady <abdo.agiry90@gmail.com>
2024-04-28 18:04:17 +0300

    add flutter screen utils to project

--- lib/store_app.dart
+++ lib/store_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_app/core/app/connectivity_controller.dart';
 import 'package:store_app/core/app/env_variables.dart';
 import 'package:store_app/core/widgets/no_network_screen/no_network_screen.dart';
@@ -12,24 +13,28 @@ class StoreApp extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (context, value, child) {
         if (value) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: EnvVariables.instance.depugMode,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
+          return ScreenUtilInit(
+            designSize: const Size(375, 812),
+            minTextAdapt: true,
+            child: MaterialApp(
+              debugShowCheckedModeBanner: EnvVariables.instance.depugMode,
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              builder: (context, child) {
+                return Scaffold(
+                  body: Builder(builder: (context) {
+                    ConnectivityController.instance.init();
+                    return child!;
+                  }),
+                );
+              },
+              home: Scaffold(
+                  appBar: AppBar(
+                title: const Text('Store App'),
+              )),
             ),
-            builder: (context, child) {
-              return Scaffold(
-                body: Builder(builder: (context) {
-                  ConnectivityController.instance.init();
-                  return child!;
-                }),
-              );
-            },
-            home: Scaffold(
-                appBar: AppBar(
-              title: const Text('Store App'),
-            )),
           );
         } else {
           return MaterialApp(
