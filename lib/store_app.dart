e51af03608f7e5b9b5c44be22d7ef61dab03353d
AbdoAlhady <abdo.agiry90@gmail.com>
2024-04-29 21:56:47 +0300

    added localization setup to app

--- lib/store_app.dart
+++ lib/store_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_app/core/app/connectivity_controller.dart';
 import 'package:store_app/core/app/env_variables.dart';
+import 'package:store_app/core/language/app_localizations_setup.dart';
 import 'package:store_app/core/routes/app_router.dart';
 import 'package:store_app/core/routes/routes.dart';
 import 'package:store_app/core/theme/app_theme.dart';
@@ -22,12 +23,21 @@ class StoreApp extends StatelessWidget {
             child: MaterialApp(
               debugShowCheckedModeBanner: EnvVariables.instance.depugMode,
               theme: AppTheme.darkTheme,
+              locale: const Locale('ar'), 
+              localeResolutionCallback: AppLocalizationsSetup.localeResolutionCallback,
+              supportedLocales: AppLocalizationsSetup.supportedLocales,
+              localizationsDelegates: AppLocalizationsSetup.localizationsDelegates,
               builder: (context, child) {
-                return Scaffold(
-                  body: Builder(builder: (context) {
-                    ConnectivityController.instance.init();
-                    return child!;
-                  }),
+                return GestureDetector(
+                  onTap: () {
+                   FocusManager.instance.primaryFocus?.unfocus();
+                  },
+                  child: Scaffold(
+                    body: Builder(builder: (context) {
+                      ConnectivityController.instance.init();
+                      return child!;
+                    }),
+                  ),
                 );
               },
               onGenerateRoute: AppRouter().onGenerateRoute,
