cea33742becdaea29cddbbd3bae6b54a6c1570ca
AbdoAlhady <abdo.agiry90@gmail.com>
2024-04-29 14:36:25 +0300

    added app theme

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/core/theme/color_extension.dart';
+import 'package:store_app/core/theme/image_extension.dart';
 
-extension Navigation on BuildContext {
+extension ContextExt on BuildContext {
+  // Color
+  MyColors get color=>Theme.of(this).extension<MyColors>()!;
+  // Image
+  MyImages get image=>Theme.of(this).extension<MyImages>()!;
+  // Navigator
   Future<void>pushNamed(String routeName,{Object? argumnets}) => Navigator.of(this).pushNamed(routeName,arguments: argumnets);
   Future<void>pushReplacementNamed(String routeName,{Object? argumnets}) => Navigator.of(this).pushReplacementNamed(routeName,arguments: argumnets);
   Future<void>pushNamedAndRemoveUntil(String routeName,{Object? argumnets}) => Navigator.of(this).pushNamedAndRemoveUntil(routeName, (route) => false,arguments: argumnets);
