9d55a3fcf2344ef478d8c412a2648bc3b7aef1ed
AbdoAlhady <abdo.agiry90@gmail.com>
2024-05-01 08:29:32 +0300

    setup bloc observe and shared prefrences

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:store_app/core/app/bloc_oserver/simple_bloc_observer.dart';
 import 'package:store_app/core/app/env_variables.dart';
 import 'package:store_app/firebase_options.dart';
 import 'package:store_app/store_app.dart';
@@ -13,6 +15,9 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  // Setup Bloc Observer
+  Bloc.observer =AppBlocObserver();
+ 
   // Lock screen to portrait
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]).then((_) {
