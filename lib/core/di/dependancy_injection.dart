cf1cfe4a75ea66276c59164d9aebcf455509fa19
AbdoAlhady <abdo.agiry90@gmail.com>
2024-05-05 10:17:46 +0300

    added dio factory % api srvice

--- lib/core/di/dependancy_injection.dart
+++ lib/core/di/dependancy_injection.dart
@@ -1,14 +1,21 @@
 import 'package:get_it/get_it.dart';
+import 'package:store_app/core/networking/dio_factory.dart';
 
 import '../app/app_cubit/app_cubit.dart';
+import '../networking/api_service.dart';
 
 GetIt getIt = GetIt.instance;
 
 Future<void> setupDependancyInjection() async {
-    await _initCore();
+  await _initCore();
 }
 
 Future<void> _initCore() async {
-// App Cubit
+  // App Cubit
   getIt.registerFactory(() => AppCubit());
+
+  // Api Service
+  getIt.registerFactory(() => ApiService(DioFactroy.getDio()));
+
+
 }
--- lib/core/helper/shared_prefrences_keys.dart
+++ lib/core/helper/shared_prefrences_keys.dart
@@ -1,4 +1,5 @@
 class SharedPrefKeys{
   static const String themeMode = "mode";
   static const String appLanguage = "appLanguage";
+  static const String accessToken = "token";
 }
