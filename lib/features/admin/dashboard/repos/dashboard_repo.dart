4d1367dfe9ab02b3bf8ee90ac50fb2dc8697243e
AbdoAlhady <abdo.agiry90@gmail.com>
2024-05-12 18:52:54 +0300

    refactor dashboard repo

--- lib/features/admin/dashboard/repos/dashboard_repo.dart
+++ lib/features/admin/dashboard/repos/dashboard_repo.dart
@@ -1,4 +1,5 @@
 import 'package:store_app/core/networking/api_result.dart';
+import 'package:store_app/core/utils/app_string.dart';
 import 'package:store_app/features/admin/dashboard/data/data_source/dashboard_data_source.dart';
 import 'package:store_app/features/admin/dashboard/data/models/category_number_response.dart';
 import 'package:store_app/features/admin/dashboard/data/models/prodcut_number_response.dart';
@@ -14,7 +15,7 @@ class DashboardRepo {
       final result = await _dashBoardDataSource.numberOfProducts();
       return ApiResult.success(result);
     } catch (e) {
-      return ApiResult.failure(e.toString());
+      return const ApiResult.failure(AppString.errorMessage);
     }
   }
 
@@ -24,7 +25,7 @@ class DashboardRepo {
       final result = await _dashBoardDataSource.numberOfCategories();
       return ApiResult.success(result);
     } catch (e) {
-      return ApiResult.failure(e.toString());
+      return const ApiResult.failure(AppString.errorMessage);
     }
   }
 
@@ -34,7 +35,7 @@ class DashboardRepo {
       final result = await _dashBoardDataSource.numberOfUsers();
       return ApiResult.success(result);
     } catch (e) {
-      return ApiResult.failure(e.toString());
+      return const ApiResult.failure(AppString.errorMessage);
     }
   }
 }
