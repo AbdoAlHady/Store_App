84eda1e74255af5c659d75fca76588f2aa4f959d
AbdoAlhady <abdo.agiry90@gmail.com>
2024-05-20 03:32:23 +0300

    provide bloc to products screen

--- lib/features/admin/add_products/presentations/screens/add_products_screen.dart
+++ lib/features/admin/add_products/presentations/screens/add_products_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:store_app/core/di/dependancy_injection.dart';
 import 'package:store_app/core/extensions/context_extension.dart';
 
 import '../../../../../core/common/widgets/admin_app_bar.dart';
+import '../bloc/get_all_admin_products_bloc/get_all_admin_products_bloc.dart';
+import '../bloc/get_all_admin_products_bloc/get_all_admin_products_event.dart';
 import '../refactors/add_products_screen_body.dart';
 
 class AddProductsScreen extends StatelessWidget {
@@ -9,12 +13,20 @@ class AddProductsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-       appBar: AdminAppBar(
-          isMain: true,
-          backgroundColor: context.color.mainColor!,
-          title: 'Products'),
-      body: const AddProductScreenBody(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<GetAllAdminProductsBloc>(
+          create: (context) => getIt<GetAllAdminProductsBloc>()
+            ..add(const GetAllAdminProductsEvent.getAdminProducts()),
+        ),
+      ],
+      child: Scaffold(
+        appBar: AdminAppBar(
+            isMain: true,
+            backgroundColor: context.color.mainColor!,
+            title: 'Products'),
+        body: const AddProductScreenBody(),
+      ),
     );
   }
 }
