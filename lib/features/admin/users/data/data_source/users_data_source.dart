014f92e82932d3629e5c86cf6301ce214f89c411
AbdoAlhady <abdo.agiry90@gmail.com>
2024-05-27 04:48:58 +0300

    added users data source and added menthod to get all users

---
+++ lib/features/admin/users/data/data_source/users_data_source.dart
@@ -0,0 +1,17 @@
+import 'package:store_app/core/networking/api_service.dart';
+import 'package:store_app/features/admin/users/data/models/get_all_users_response.dart';
+
+import '../../../../../core/networking/graphql_quries/admin/users_admin_queries.dart';
+
+class UsersDataSource {
+  final ApiService _apiService;
+
+  UsersDataSource(this._apiService);
+
+  // Get All Users
+  Future<GetAllUsersResponse> getAllUsers() async {
+    final response =
+        await _apiService.getAllUsers(UsersAdminQueries().getAllUsersQuery());
+    return response;
+  }
+}
