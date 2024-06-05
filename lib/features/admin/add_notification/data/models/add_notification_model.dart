1a81cfabbee793e6558bf793a8b39f9f5a98e217
AbdoAlhady <abdo.agiry90@gmail.com>
2024-06-05 13:41:24 +0300

    added hive notification model

---
+++ lib/features/admin/add_notification/data/models/add_notification_model.dart
@@ -0,0 +1,15 @@
+import 'package:hive/hive.dart';
+part 'add_notification_model.g.dart';
+@HiveType(typeId: 0)
+class AddNotificationModel extends HiveObject {
+  @HiveField(0)
+  String title;
+  @HiveField(1)
+  String body;
+  @HiveField(2)
+  String productId;
+  @HiveField(3)
+  final DateTime createdAt;
+
+  AddNotificationModel({required this.createdAt, required this.title, required this.body, required this.productId});
+}
