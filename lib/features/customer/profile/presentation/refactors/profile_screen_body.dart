edf04521709d72ba3d204661f016be5aff3ff544
AbdoAlhady <abdo.agiry90@gmail.com>
2024-06-11 01:27:06 +0300

    profile language setting

--- lib/features/customer/profile/presentation/refactors/profile_screen_body.dart
+++ lib/features/customer/profile/presentation/refactors/profile_screen_body.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:store_app/core/common/widgets/text_app.dart';
+import 'package:store_app/core/extensions/context_extension.dart';
+import 'package:store_app/core/helper/spacing.dart';
+import 'package:store_app/core/language/lang_keys.dart';
+import 'package:store_app/core/styles/fonts/font_wight_helper.dart';
+import 'package:store_app/features/customer/profile/presentation/widgets/change_language.dart';
 import 'package:store_app/features/customer/profile/presentation/widgets/user_profile_info.dart';
 
 class ProfileScreenBody extends StatelessWidget {
@@ -8,12 +14,28 @@ class ProfileScreenBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 15.w,vertical: 10.h),
-      child: const SingleChildScrollView(
+      padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 10.h),
+      child: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // User Profile Inf,o
-            Center(child: UserProfileInfo()),
+            const Center(child: UserProfileInfo()),
+            verticalSpace(20.h),
+
+            // Title,
+            TextApp(
+              text: context.translator(LangKeys.applicationFeatures),
+              style: context.textStyle.copyWith(
+                fontSize: 18.sp,
+                fontWeight: FontWeightHelper.bold,
+              ),
+            ),
+            verticalSpace(30.h),
+
+            // Language
+            const ChangeLanguage(),
+
           ],
         ),
       ),
