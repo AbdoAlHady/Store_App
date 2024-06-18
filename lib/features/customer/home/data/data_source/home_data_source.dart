618dd339e1515174ed6e3cc0f5d3053746d049ce
AbdoAlhady <abdo.agiry90@gmail.com>
2024-06-18 22:28:02 +0300

    home  categories data source && repo

--- lib/features/customer/home/data/data_source/home_data_source.dart
+++ lib/features/customer/home/data/data_source/home_data_source.dart
@@ -2,6 +2,8 @@ import 'package:store_app/core/networking/graphql_quries/user/home_queries.dart'
 import 'package:store_app/features/customer/home/data/models/banner_response.dart';
 
 import '../../../../../core/networking/api_service.dart';
+import '../../../../../core/networking/graphql_quries/admin/add_categories_queries.dart';
+import '../../../../admin/add_categories/data/models/categories_response_model.dart';
 
 class HomeDataSource {
   final ApiService _apiService;
@@ -12,6 +14,13 @@ class HomeDataSource {
   Future<BannersResponse> getAllBanners() async {
     final response =
         await _apiService.getAllBanners(Homequeries().bannersQuery());
+    return response;
+  }
+
+  // Get All Categories
+  Future<CategoriesResponse> getAllCategoriesAdmin() async {
+    final response = await _apiService
+        .getAllCategories(CategoriesQueries().getAllCategoriesQuery());
     return response;
   }
 }
--- lib/features/customer/home/data/repos/home_repo.dart
+++ lib/features/customer/home/data/repos/home_repo.dart
@@ -4,6 +4,8 @@ import 'package:store_app/core/utils/app_string.dart';
 import 'package:store_app/features/customer/home/data/data_source/home_data_source.dart';
 import 'package:store_app/features/customer/home/data/models/banner_response.dart';
 
+import '../../../../admin/add_categories/data/models/categories_response_model.dart';
+
 class HomeRepo {
   final HomeDataSource _dataSource;
 
@@ -16,6 +18,16 @@ class HomeRepo {
       return ApiResult.success(result);
     } catch (e) {
       debugPrint(' ==[Home Repo Get All Banners Error : ${e.toString()}]== ');
+      return const ApiResult.failure(AppString.errorMessage);
+    }
+  }
+  // Get all categories
+  Future<ApiResult<CategoriesResponse>> getAllCategories()async {
+    try {
+      final result = await _dataSource.getAllCategoriesAdmin();
+      return ApiResult.success(result);
+    } catch (e) {
+      debugPrint("===[Home Repo Categories Customer Error: $e]===");
       return const ApiResult.failure(AppString.errorMessage);
     }
   }
